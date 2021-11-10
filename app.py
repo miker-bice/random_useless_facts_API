1a1fc022a568e3214e42783c4a412ad986ba68bd
miker-bice <mikejerrson.galindez@gmail.com>
2021-11-10 17:46:24 +0800

    almost done, code refactoring needed

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 import requests
+from requests.exceptions import HTTPError
 
-response = requests.get('https://uselessfacts.jsph.pl/random.json?language=en')
-print(response.json())
+try:
+    response = requests.get('https://uselessfacts.jsph.pl/random.json?language=en')
+    response.raise_for_status()
 
-# I need to extract the text inside the json response from the API endpoint
+    jsonResponse = response.json()
+    print(f"""THE RANDOM USELESS FACT: 
+            {jsonResponse['text']}""")
+    print(f"source: {jsonResponse['source']}")
+except HTTPError as http_err:
+    print(f'Http error occurred: {http_err}')
+except Exception as err:
+    print(f'Other error occurred: {err}')
+
+
